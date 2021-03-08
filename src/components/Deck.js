ec6ec9472ab7f805669eaa8fad844d78c436076e
Anselmos <anselmos@users.noreply.github.com>
2021-03-08 15:25:58 +0100

    Adds buttons instead of text.

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, Button, View} from 'react-native';
 // Individual deck card.
 /**
  The deck title
@@ -7,15 +7,14 @@ Number of cards in the deck
 Option to start a quiz for that deck
 Option to add a new question to the deck
  */
-export default function Deck({title, number_of_cards, quiz_callback, new_question_callback}){
-    // TODO  quiz_callback, new_question_callback
-    number_of_cards = 1;
+
+export default function Deck({deck_id, title, number_of_cards, quiz_callback, new_question_callback}){
   return (
     <View>
       <Text>Deck Title: {title} </Text>
       <Text>Number of cards {number_of_cards} </Text>
-      <Text>Option to start a quiz for that deck</Text>
-      <Text>Option to add a new question to the deck</Text>
+      <Button onPress={quiz_callback.bind(this, deck_id)}>Start Quiz</Button>
+      <Button onPress={new_question_callback.bind(this, deck_id)}>Add new Question to Deck</Button>
     </View>
   );
 }
